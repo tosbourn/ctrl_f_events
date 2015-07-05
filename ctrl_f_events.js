cb8f318d0f544022fd2de12c53255ee37653c897
tosbourn <toby.osbourn@googlemail.com>
2015-07-05 19:02:27 +0100

    protect the function

--- ctrl_f_events.js
+++ ctrl_f_events.js
@@ -1,33 +1,34 @@
-var CTRL = 17,
-    CMD_SAFARI_CHROME = 91,
-    CMD_FIREFOX = 224,
-    F = 70;
+(function(){
+  var CTRL = 17,
+      CMD_SAFARI_CHROME = 91,
+      CMD_FIREFOX = 224,
+      F = 70;
 
-var isReserved = false;
+  var isReserved = false;
 
-var modifierKeyPressed = function(keyCode) {
-  return (keyCode == CTRL || keyCode == CMD_SAFARI_CHROME || keyCode == CMD_FIREFOX);
-}
-
-window.onkeyup=function(ev){
-  if(modifierKeyPressed(ev.which)){
-    isReserved = false;
+  var modifierKeyPressed = function(keyCode) {
+    return (keyCode == CTRL || keyCode == CMD_SAFARI_CHROME || keyCode == CMD_FIREFOX);
   }
-}
-window.onkeydown=function(ev){
-  if(modifierKeyPressed(ev.which)) {
-    isReserved = true;
-    window.setInterval(function(){
+
+  window.onkeyup=function(ev){
+    if(modifierKeyPressed(ev.which)){
       isReserved = false;
-    }, 1000);
-  }
-
-  if((isReserved==true)&&(ev.which == F)) {
-    if (typeof _trackEvent === 'function') {
-      _trackEvent("Browser Action", "Internal Page Search");
-    } else {
-      console.log("Is Google Analytics correctly set up on this page?");
     }
   }
-}
+  window.onkeydown=function(ev){
+    if(modifierKeyPressed(ev.which)) {
+      isReserved = true;
+      window.setInterval(function(){
+        isReserved = false;
+      }, 1000);
+    }
 
+    if((isReserved==true)&&(ev.which == F)) {
+      if (typeof _trackEvent === 'function') {
+        _trackEvent("Browser Action", "Internal Page Search");
+      } else {
+        console.log("Is Google Analytics correctly set up on this page?");
+      }
+    }
+  }
+})();
